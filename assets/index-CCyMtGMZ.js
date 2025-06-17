248a69ab678cd49820b0e663183af753d1feb97d
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2025-06-17 18:18:13 -0300

    Updates

---
+++ assets/index-CCyMtGMZ.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-VU69Q7-h.js";var a=globalThis!=null&&globalThis.document?o.useLayoutEffect:o.useEffect;export{a as u};
--- assets/index-CVJ9RJH-.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-CMsgf0my.js";import"./index-CBjmDVyM.js";var d=a;export{d as default};
