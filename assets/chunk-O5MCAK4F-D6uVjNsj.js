8079dd80cccbdb47089299234a4dd7ceaa7b5898
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2025-06-17 17:50:48 -0300

    Updates

---
+++ assets/chunk-O5MCAK4F-D6uVjNsj.js
@@ -0,0 +1,1 @@
+import{u as d}from"./chunk-3BLMMOBU-3kgIo_ag.js";import{M as p,Y as u,j as i,J as x}from"./index-CBjmDVyM.js";var s=p((e,a)=>{const{as:r,children:t,className:l,...m}=e,{slots:f,classNames:o}=d(),n=u(a),c=r||"footer";return i.jsx(c,{ref:n,className:f.footer({class:x(o==null?void 0:o.footer,l)}),...m,children:t})});s.displayName="HeroUI.ModalFooter";var R=s;export{R as m};
---
+++ assets/features-animation-CMsgf0my.js
@@ -0,0 +1,1 @@
+import{bx as a,by as e,bz as n}from"./index-CBjmDVyM.js";const s={renderer:n,...e,...a};export{s as d};
