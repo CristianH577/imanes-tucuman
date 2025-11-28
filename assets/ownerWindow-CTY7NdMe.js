d18eaf3f7a2ca80b459f55b374a01b053f72aa19
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2025-11-28 18:22:30 -0300

    Updates

---
+++ assets/ownerWindow-CTY7NdMe.js
@@ -0,0 +1,1 @@
+import{aG as n}from"./index-Bqlzsw-l.js";function t(o){return n(o).defaultView||window}export{t as o};
--- assets/ownerWindow-CmaziCEf.js
+++
@@ -1,1 +0,0 @@
-import{aG as n}from"./index-DSDHqNJP.js";function t(o){return n(o).defaultView||window}export{t as o};
