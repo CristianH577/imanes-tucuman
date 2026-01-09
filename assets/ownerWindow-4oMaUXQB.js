432945c75c10a915f0474e84c60b99077614a510
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2026-01-09 19:51:08 -0300

    Updates

--- assets/ownerWindow-4oMaUXQB.js
+++
@@ -1,1 +0,0 @@
-import{aG as n}from"./index-B_8SBfU6.js";function t(o){return n(o).defaultView||window}export{t as o};
---
+++ assets/ownerWindow-wForbMAC.js
@@ -0,0 +1,1 @@
+import{ae as n}from"./index-ohK6KTTy.js";function t(o){return n(o).defaultView||window}export{t as o};
