a2a2830f9b4f80e8db817ca965d15b0ae6962950
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2026-02-25 13:23:20 -0300

    Updates

--- assets/ownerWindow-BUvpPipP.js
+++
@@ -1,1 +0,0 @@
-import{ae as n}from"./index-BbZaWXP8.js";function t(o){return n(o).defaultView||window}export{t as o};
---
+++ assets/ownerWindow-cYYYQEUV.js
@@ -0,0 +1,1 @@
+import{ae as n}from"./index-C6z8gMta.js";function t(o){return n(o).defaultView||window}export{t as o};
