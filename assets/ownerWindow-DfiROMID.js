d8eae48572b939ffe08d8ec2610b39d013131fb5
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2026-02-27 20:30:25 -0300

    Updates

---
+++ assets/ownerWindow-DfiROMID.js
@@ -0,0 +1,1 @@
+import{ae as n}from"./index-DVZ5-4wt.js";function t(o){return n(o).defaultView||window}export{t as o};
--- assets/ownerWindow-cYYYQEUV.js
+++
@@ -1,1 +0,0 @@
-import{ae as n}from"./index-C6z8gMta.js";function t(o){return n(o).defaultView||window}export{t as o};
