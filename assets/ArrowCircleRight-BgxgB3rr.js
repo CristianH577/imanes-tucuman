fbdb5117327b50a0cdf742a14c0eebf4351aeb99
CristianH577 <Cristian.herrera07@hotmail.com>
2026-09-08 22:45:35 -0300

    Updates

--- assets/ArrowCircleRight-BgxgB3rr.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as r}from"./index-CSxP3c8p.js";const t=o(r.jsx("path",{d:"M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12s4.48 10 10 10 10-4.48 10-10m-10 1H8v-2h4V8l4 4-4 4z"}));export{t as A};
--- assets/ownerWindow-DEQ28U7x.js
+++
@@ -1,1 +0,0 @@
-import{bq as n}from"./index-CSxP3c8p.js";function t(o){return n(o).defaultView||window}export{t as o};
