20956f67af8337628a2457aa2f9ff6900d453725
CristianH577 <Cristian.herrera07@hotmail.com>
2026-09-08 22:22:16 -0300

    Updates

---
+++ assets/ownerWindow-DEQ28U7x.js
@@ -0,0 +1,1 @@
+import{bq as n}from"./index-CSxP3c8p.js";function t(o){return n(o).defaultView||window}export{t as o};
--- assets/ownerWindow-Dohm7acn.js
+++
@@ -1,1 +0,0 @@
-import{bp as n}from"./index-4f1gvZnp.js";function t(o){return n(o).defaultView||window}export{t as o};
